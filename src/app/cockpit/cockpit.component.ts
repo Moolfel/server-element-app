68ddba36d7d58d2d3fc8c52bb52247e681b335fe
Moolfel <negide3@gmail.com>
2021-10-17 22:36:17 +0200

    custom

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -7,12 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CockpitComponent implements OnInit {
 
-  serverCreated;
-  blueprintCreated;
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
+  newServerName = '';
+  newServerContent = '';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddServer(){
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    })
+  }
+
+  onAddBlueprint(){
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    })
+  }
 }
